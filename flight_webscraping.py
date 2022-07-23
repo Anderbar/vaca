2955d30a862d73ccedaa32cbfa19d6b9f9389e85
ananan02 <109250352+ananan02@users.noreply.github.com>
2022-07-23 13:41:49 -0400

    Add files via upload

--- flight_webscraping.py
+++ flight_webscraping.py
@@ -1,23 +1,51 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jul 16 12:40:10 2022
+Created on Thu Jul 21 09:41:19 2022
 
 @author: ananyayadav
 """
 
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import airportsdata
+import geopy.distance
+from geopy.geocoders import Nominatim
 
+def find_airports(city, state):
+    #find coordinates of starting location
+    airports = airportsdata.load('IATA')
+    geolocator = Nominatim(user_agent="Vaca")
+    g_loc = geolocator.geocode(city + " " + state)
+    loc_coords = (g_loc.latitude, g_loc.longitude)
+
+    #search for international airports within 2.5 lat/lon from starting location   
+    dict1 = {}     
+    for x in airports.keys():
+        if "International" in airports[x]['name']: 
+            if airports[x]['lat'] >= (loc_coords[0] - 2.5) and airports[x]['lat'] <= (loc_coords[0] + 2.5):
+                if airports[x]['lon'] >= (loc_coords[1] - 2.5) and airports[x]['lon'] <= (loc_coords[1] + 2.5):
+                    airport_coords = (airports[x]['lat'], airports[x]['lon'])
+                    dist = geopy.distance.geodesic(loc_coords, airport_coords).miles
+                    dict1.update({dist: x})
+    #create list of airports in order of distance from starting location
+    airport_list = []
+    for i in sorted(dict1.keys()):
+        airport_list.append(dict1[i])
+    print(airport_list)
+    return airport_list
+        
 
 def make_flight_url(dep_loc, arr_loc, dep_date, arr_date):
     browser = "https://www.kayak.com/flights/" + dep_loc + "-" + arr_loc + "/" + dep_date + "/" + arr_date + "?sort=price_a&fs=stops=~0"
     return browser
 
-
-def close_popup():
-        """ Close popups that appear on flight pages"""
+#Close popups that appear in flight pages
+def close_popup(driver):
         try:
             popUp_button = driver.find_elements_by_xpath('//button[contains(@id, "dialog-close") and contains(@class, "Button-No-Standard-Style close ")]')
             popUp_button[5].click()
@@ -25,12 +53,17 @@ def close_popup():
         except:
             pass
 
-#details for flight to travel destination (includes price)
-def scrape_start_flight_data():
-    
-    price = driver.find_element_by_xpath('//span[@class="price-text"]').text
+#check if kayak was able to find results
+def info_found(driver):
+    try:
+        wait_object(driver, '//div[@class = "col col-banner center"]')
+        return False
+    except:
+        return True
 
-    
+#details for flight to travel destination (includes price)
+def scrape_start_flight_data(driver):
+    price = driver.find_element_by_xpath('//span[@class="price option-text"]/span[1]').text
     duration = driver.find_element_by_xpath('//div[@class="section duration allow-multi-modal-icons"]/div[1]').text
     airport_str = driver.find_element_by_xpath('//div[@class="section duration allow-multi-modal-icons"]/div[2]').text
     start_airport = format_airports(airport_str)[0]
@@ -40,51 +73,66 @@ def scrape_start_flight_data():
     return duration, start_airport, end_airport, time, airline, price
 
 
-#details for flight to starting location
-def scrape_end_flight_data():
-
+#details for flight to starting location (includes url to kayak options)
+def scrape_end_flight_data(driver):
     duration = driver.find_element_by_xpath('//li[@class="flight "]//div[@class="section duration allow-multi-modal-icons"]/div[1]').text
     airport_str = driver.find_element_by_xpath('//li[@class="flight "]//div[@class="section duration allow-multi-modal-icons"]/div[2]').text
     start_airport = format_airports(airport_str)[0]
     end_airport = format_airports(airport_str)[1]
     time = driver.find_element_by_xpath('//li[@class="flight "]//div[@class="section times"]/div[1]').text
     airline = driver.find_element_by_xpath('//li[@class="flight "]//div[@class="section times"]/div[2]').text
-    return duration, start_airport, end_airport, time, airline
+    url = driver.current_url
+    return duration, start_airport, end_airport, time, airline, url
     
-
+#reformat string with airports information for a flight
 def format_airports(airports):
     l = airports.split("\n")
     l.pop(1)
     return l
 
-#Inputs
-dep_loc = "NYC"
+#chrome_options = Options()
+#chrome_options.add_argument("--headless")
+#chrome_options.add_argument('window-size=1200x600')
+#driver = webdriver.Chrome(options=chrome_options)
+
+def wait_object(driver, xpath):
+    return WebDriverWait(driver, 30).until(EC.presence_of_all_elements_located((By.XPATH, xpath)))
+    
+def get_info(dep_city, dep_state, arr_code, dep_date, arr_date):
+    
+    driver = webdriver.Chrome()
+    airport_list = find_airports(dep_city, dep_state)
+    for x in airport_list:
+        driver.get(make_flight_url(x, arr_code, dep_date, arr_date))
+        close_popup(driver)
+        #kayak contain flight info, gather data and end loop
+        if info_found(driver):
+            flight1 = scrape_start_flight_data(driver)
+            flight2 = scrape_end_flight_data(driver)
+            print(flight1)
+            print(flight2)
+            driver.quit()
+            break
+        #kayak has no flights for this airport, look at next closest airport
+        else:
+            print("No Info Found")
+            continue
+        
+#need full city and state
+dep_city = "Shelton"
+dep_state = "Connecticut"
 arr_loc = "SFO"
-dep_date = "2022-08-15"
-arr_date = "2022-08-22"
-
-#Launch Kayak website
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get(make_flight_url(dep_loc, arr_loc, dep_date, arr_date))
-time.sleep(1)
-
-close_popup()
+dep_date = "2022-08-06"
+arr_date = "2022-08-08"
 
 
-#div[@aria-label='Result number 1']
+get_info(dep_city, dep_state, arr_loc, dep_date, arr_date)
 
-flight1 = scrape_start_flight_data()
-flight2 = scrape_end_flight_data()
 
-url = driver.current_url
 
-print(flight1)
-print(flight2)
 
-#driver.quit()
 
-#issues: nonstop, +1, # of travelers, city codes, links
-#add ons-> options for flight (nonstop, 1 stop, 2 stop+), # of travelers
 
-#city codes -> for final destination, manually input codes
-#for starting, need to figure out how to get airports
+
+    
+
--- hotel_webscraping.py
+++ hotel_webscraping.py
@@ -7,11 +7,14 @@ Created on Sat Jul 16 17:35:27 2022
 """
 
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
-def close_popup():
+
+def close_popup(driver):
         """ Close popups that appear on flight pages"""
         try:
             popUp_button = driver.find_elements_by_xpath('//button[contains(@id, "dialog-close") and contains(@class, "Button-No-Standard-Style close ")]')
@@ -28,52 +31,93 @@ def update_url(url, dep, arr, stars):
     new_url = new_url + "?sort=price_a&fs=stars=*" + str(stars)
     return new_url
 
-#Launch Kayak website
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("https://www.kayak.com/stays")
-time.sleep(1)
-
-close_popup()
-
-stars = 3
-dest = "San Francisco, California"
-dep_date = "2022-08-15"
-arr_date = "2022-08-22"
-
-#Only insert location into search option -> each location has unique code given by Kayak
-#All other filters will be done through URL
-elem = driver.find_element_by_xpath('//input[@class = "k_my-input"]')
-elem.send_keys(dest)
-time.sleep(1)
-elem.send_keys(Keys.ARROW_DOWN)
-time.sleep(1)
-elem.send_keys(Keys.RETURN)
-driver.find_element_by_xpath('//button[@type = "submit"]').click()
-time.sleep(1)
-url = driver.current_url
-new_url = update_url(url, dep_date, arr_date, stars)
-print(new_url)
-time.sleep(10)
+def wait_object(driver, xpath):
+    return WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, xpath)))
 
 
-driver.quit()
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get(new_url)
-time.sleep(5)
-
-price = driver.find_element_by_xpath('//div[@class = "zV27-price"]').text
-name = driver.find_element_by_xpath('//div[@class = "FLpo-hotel-name"]').text
-stars = driver.find_element_by_xpath('//div[@class = "FLpo-stars"]/div[1]').get_attribute("class")
-rating = stars[1]
-if "*" in price:
-    new_price = price[:-1]
-    price = new_price
     
-print(price)
-print(name)
-print(stars[1])
+def get_more_info(driver):
+    #open new window with more info
+    wait_object(driver, '//div[@class = "FLpo-reviews"]')
+    button = driver.find_element_by_xpath('//div[@class = "FLpo-reviews"]')
+    button.click()
+    
+    time.sleep(1)
+    
+    #switch driver focus to new window
+    curr = driver.current_window_handle
+    other = driver.window_handles
+    for x in other:
+        if x != curr:
+            driver.switch_to.window(x)
+            break
+    
+    #Scroll up to the top of the page
+    htmlelement= driver.find_element_by_tag_name('html')
+    time.sleep(1)
+    htmlelement.send_keys(Keys.HOME)
+    time.sleep(0.5)
+    
+    #Retrieve info
+    info_url = driver.current_url
+    info_dict = {}
+    info_dict.update({"url": info_url.replace("#reviews", "")})
+    
+    wait_object(driver, '//div[@class = "c3xth-price-info-wrapper"]')
+    
+    name = driver.find_element_by_xpath('//div[@class = "c3xth-title"]').text
+    address = driver.find_element_by_xpath('//div[@class = "c3xth-address"]').text
+    below_info = driver.find_element_by_xpath('//div[@class = "c3xth-info-below-address"]').text
+    below_info = below_info.split("\n")
+    price = driver.find_element_by_xpath('//div[@class = "c3xth-price-info-wrapper"]').text
+    price = price.split("\n")
+    info_dict.update({"name": name})
+    info_dict.update({"address": address})
+    info_dict.update({"cust_rating": below_info[0]})
+    info_dict.update({"rating_level": below_info[1]})
+    info_dict.update({"num_reviews": below_info[2]})
+    info_dict.update({"price": price[0]})
+    info_dict.update({"website": price[1]})
 
-driver.quit()
+    driver.quit()
+    return info_dict   
+
+
+def get_started(stars, dest, dep_date, arr_date):
+    #Launch Kayak website
+    driver = webdriver.Chrome()
+    driver.get("https://www.kayak.com/stays")
+    time.sleep(1)
+
+    close_popup(driver)
+
+    #Only insert location into search option -> each location has unique code given by Kayak
+    #All other filters will be done through URL
+    elem = driver.find_element_by_xpath('//input[@class = "k_my-input"]')
+    elem.send_keys(dest)
+    time.sleep(1)
+    elem.send_keys(Keys.ARROW_DOWN)
+    time.sleep(1)
+    elem.send_keys(Keys.RETURN)
+    driver.find_element_by_xpath('//button[@type = "submit"]').click()
+    time.sleep(1)
+    #Create new url with city code and launch
+    url = driver.current_url
+    new_url = update_url(url, dep_date, arr_date, stars)
+    time.sleep(3)
+    driver.get(new_url)
+    #Allow page to load
+    time.sleep(15)
+    
+    return(get_more_info(driver))
+
+stars = 4
+dest = "New York, New York, USA"
+dep_date = "2022-08-06"
+arr_date = "2022-08-08"
+
+info = get_started(stars, dest, dep_date, arr_date)
+print(info)
+
 
 
